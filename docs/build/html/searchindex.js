e931bd52d48709e0b6c6d186d8f5d45503638d53
aniket-codedater <aniketkok10@gmail.com>
2017-05-26 01:08:41 +0530

    Removed docs from ignore list

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to omnidrive-control\u2019s documentation!"],titleterms:{control:0,document:0,indic:0,omnidr:0,tabl:0,welcom:0}})
